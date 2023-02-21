40c31bd8eadfbecf1041d8a0436665c10ec6cfbf
Юрий <yshulga9@gmail.com>
2023-02-21 10:19:22 +0300

    добавил класс Money

---
+++ Money.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCs4
+{
+    internal class Money
+    {
+        private int rubl;
+        private int penny;
+        public int Rubl {
+            get { 
+                return rubl;
+            }
+            set { 
+                if (value >=0) rubl = value;
+                else rubl = 0;
+            }
+        }
+        public int Penny
+        {
+            get { 
+                return penny;
+            }
+            set { 
+                if (value>=0) penny = value;
+                else penny = 0;
+            }
+        }
+    }
+}
